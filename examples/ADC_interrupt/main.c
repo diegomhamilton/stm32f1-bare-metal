31adcdf3b67f34a06ecd6c2c338f80e0c18e7a56
Diego Hamilton <diegohamilton26@gmail.com>
2020-07-19 11:57:47 -0300

    Add volatile to variable used in interrupt

--- examples/ADC_interrupt/main.c
+++ examples/ADC_interrupt/main.c
@@ -1,18 +1,14 @@
 #include "stm32f103xb.h"
 #include "adc.h"
 
-uint8_t adc_conv_ready = 0;
-uint16_t conversion_result = 0;
+volatile uint8_t adc_conv_ready = 0;
+volatile uint16_t conversion_result = 0;
 
 void ADC1_2_IRQHandler(void) {
 	if (ADC_EVT_EOC) {
 		adc_conv_ready = 1;
 		conversion_result = adc_read();
-		if (conversion_result > 2048) {
-			GPIOC->BSRR |= GPIO_BSRR_BR13;
-		} else {
-			GPIOC->BSRR |= GPIO_BRR_BR13;
-		}	}
+	}
 }
 
 int
@@ -20,18 +16,18 @@ main(void) {
 	adc_init();
 
 	/* GPIO settings */
-	adc_set_gpio(0);
+	adc_set_gpio(4);
 	
 	/* Channel 0 Single Conversion */
 	ADC_CONV_SINGLE_MODE();
 	ADC_CONV_RIGHT_ALIGN();
 	// ADC1->SMPR2 &= ~(ADC_SMPR2_SMP0);					// Set 1.5 cycle of sample time (SMP0 = 000)
 	ADC_GRP_NUM_OF_CONV(1);
-	ADC_GRP_1ST_CONV_CHAN(0);
+	ADC_GRP_1ST_CONV_CHAN(4);
 
 	/* Enable EOC interrupt */
 	adc_enable_EOC_irq();
-	GPIOC->BSRR |= GPIO_BRR_BR13;
+	GPIOC->BSRR = GPIO_BSRR_BS13;
 
 	while(1) {
 		ADC1->CR2 |= ADC_CR2_ADON;
@@ -39,9 +35,9 @@ main(void) {
 		adc_conv_ready = 0;
 
 		if (conversion_result > 2048) {
-			GPIOC->BSRR |= GPIO_BSRR_BR13;
+			GPIOC->BRR = GPIO_BRR_BR13;
 		} else {
-			GPIOC->BSRR |= GPIO_BRR_BR13;
+			GPIOC->BSRR = GPIO_BSRR_BS13;
 		}
 	}
 }
