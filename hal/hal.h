4cba411627a40ec0c288f53d95ac2a3887408389
Hermano Cabral <hcabral00@gmail.com>
2020-08-09 16:55:27 -0300

    Added initial support for SPI

--- hal/hal.h
+++ hal/hal.h
@@ -5,12 +5,14 @@
 #include "uart.h"
 #include "dma.h"
 #include "adc.h"
+#include "spi.h"
 
 #define hal_init() do {				\
         gpio_init();				\
 	uart_init();				\
 	dma_init();				\
         adc_init();                 \
+        spi_init();                 \
     } while(0)
 
 #endif /* HAL_H */
--- hal/hal.mk
+++ hal/hal.mk
@@ -4,3 +4,4 @@ include $(BASE_DIR)/hal/GPIO/gpio.mk
 include $(BASE_DIR)/hal/ADC/adc.mk
 include $(BASE_DIR)/hal/UART/uart.mk
 include $(BASE_DIR)/hal/DMA/dma.mk
+include $(BASE_DIR)/hal/SPI/spi.mk
